aa6ec5052b458f8265d8bdf8cece4d011af564d7
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-22 18:35:04 +0100

    link file script to file php for tranfer data and print on html

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
-
-createApp({
+const { createApp } = Vue;
+const endpoint = 'http://localhost/PHP-TODO-LIST-JSON/api/index.php'
+const app = createApp({
     data: () => ({
 
-    })
-}).mount('#app')
+        tasks: []
+
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data
+        })
+    }
+})
+app.mount('#app')
