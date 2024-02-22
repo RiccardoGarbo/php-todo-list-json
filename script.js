1b86acc16e393e2121bd3df6d09b3a6d6889f96a
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-22 22:45:36 +0100

    add element on page

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ const { createApp } = Vue;
 const endpoint = 'http://localhost/PHP-TODO-LIST-JSON/api/index.php'
 const app = createApp({
     data: () => ({
-        tasks: []
+        tasks: [],
+
+        newTask: {
+            "id": 5,
+            "text": "",
+            "done": false
+        }
+
 
     }),
+    methods: {
+        addTask() {
+            const data = { 'task': this.newTask }
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+            axios.post(endpoint, data, config)
+
+        }
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data
